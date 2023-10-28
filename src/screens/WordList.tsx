b809a1aa62933dea697d8e9b58cd325c1e81c05f
slaven.cvijetic <slaven.cvijetic@noser.com>
2023-10-28 14:06:40 +0200

    Add search bar to word list

--- src/screens/WordList.tsx
+++ src/screens/WordList.tsx
@@ -1,23 +1,46 @@
 import {
   FlatList,
   ListRenderItemInfo,
+  NativeSyntheticEvent,
   StyleSheet,
   Text,
+  TextInput,
+  TextInputChangeEventData,
   TouchableOpacity,
   View,
 } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import c1Data from '../../data/C1_english_vocabulary.json';
 import { WordDetail } from './WordDetail';
+import { useState } from 'react';
 
 export const WordList = ({ route }) => {
   const { color } = route.params;
   const navigation = useNavigation();
+  const [filteredWords, setFilteredWords] = useState(c1Data);
+
+  const handleSetFilteredWords = (
+    event: NativeSyntheticEvent<TextInputChangeEventData>
+  ): void => {
+    if (event.nativeEvent.text.length > 1) {
+      let filtered = c1Data.filter((item: WordDetail) =>
+        item.word.startsWith(event.nativeEvent.text)
+      );
+      setFilteredWords(filtered);
+    } else {
+      setFilteredWords(c1Data); // reset
+    }
+  };
 
   return (
     <View style={styles.container}>
+      <TextInput
+        style={styles.input}
+        onChange={handleSetFilteredWords}
+        underlineColorAndroid="transparent"
+      />
       <FlatList
-        data={c1Data}
+        data={filteredWords}
         renderItem={({ item }: ListRenderItemInfo<WordDetail>) => (
           <View style={styles.item}>
             <TouchableOpacity
@@ -40,6 +63,14 @@ const styles = StyleSheet.create({
     display: 'flex',
     flexDirection: 'column',
     justifyContent: 'space-around',
+  },
+  input: {
+    borderWidth: 0.1,
+    borderStyle: 'solid',
+    borderRadius: 10,
+    marginBottom: 24,
+    paddingHorizontal: 12,
+    fontSize: 18,
   },
   item: {
     paddingVertical: 10,
