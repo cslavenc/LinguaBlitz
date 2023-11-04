e40274c7552f4a02c1bf1077b9ceb79a8cdace1b
slaven.cvijetic <slaven.cvijetic@noser.com>
2023-11-04 12:57:52 +0100

    Change size of magnifying glass

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -57,8 +57,8 @@ export const SpeechBubbleIcon: React.FC<Icon> = ({ color }) => {
 export const SearchIcon = () => {
   return (
     <Svg
-      width="24"
-      height="24"
+      width="20"
+      height="20"
       viewBox="0 0 24 24"
       fill="none"
       xmlns="http://www.w3.org/2000/svg">
