74d8d36519e176c4e24c757058cd6334d009d2be
slaven.cvijetic <slaven.cvijetic@noser.com>
2024-01-06 10:33:00 +0100

    Make descriptions shorter

--- src/screens/book/BookData.ts
+++ src/screens/book/BookData.ts
@@ -3,21 +3,21 @@ import { BookItem } from './BookItem';
 export const BookData: BookItem[] = [
   {
     description:
-      "A three-level (B1+ to C1) integrated skills course for higher education students at university or on foundation courses. The C1 Advanced Student's Book consolidates academic study skills. Students' analytical skills are challenged with an increased range of authentic written and spoken academic texts.",
+      "A three-level (B1+ to C1) integrated skills course for higher education students at university or on foundation courses. The C1 Advanced Student's Book consolidates academic study skills.",
     imageUrl: require('../../../assets/lb-recommended-book-c1.jpg'),
     bookUrl:
       'https://www.amazon.com/Cambridge-Academic-English-Advanced-Students/dp/0521165210/ref=sr_1_1?keywords=cambridge+english+c1&sr=8-1',
   },
   {
     description:
-      'Improve your English in all the skills (vocabulary, grammar, writing, speaking and listening) and pass your official Cambridge C1, IELTS, Trinity or TOEFL English exam. This self-study book will teach you essential C1 English and help you achieve your C1 Advanced certificate.',
+      'Improve your English in all the skills (vocabulary, grammar, writing, speaking and listening) and pass your official Cambridge C1, IELTS, Trinity or TOEFL English exam.',
     imageUrl: require('../../../assets/lb-recommended-book-self-study.jpg'),
     bookUrl:
       'https://www.amazon.com/Activate-Advanced-C1-Complete-Self-Study/dp/8412214358/ref=sr_1_6?keywords=cambridge+english+c1+vocabulary&sr=8-6',
   },
   {
     description:
-      "A wonderfully colourful and clear series of books to show you 'An Ocean of English Idioms'. Explore the ocean with over 200 essential idioms in each book. Each idiom has a clear and simple definition, 3 examples and a picture to make sure the meaning is very clear for you.",
+      "A wonderfully colourful and clear series of books to show you 'An Ocean of English Idioms'. Explore the ocean with over 200 essential idioms in each book.",
     imageUrl: require('../../../assets/lb-recommended-book-idioms.jpg'),
     bookUrl:
       'https://www.amazon.com/Ocean-English-Idioms-Book/dp/B09RM5HX6F/ref=sr_1_7?keywords=cambridge+english+c1+idioms&sr=8-7',
