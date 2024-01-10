0898b244599b89849094f1cc9617e2e536dc795e
slaven.cvijetic <slaven.cvijetic@noser.com>
2024-01-10 08:04:06 +0100

    Add more colors

--- src/theme.ts
+++ src/theme.ts
@@ -2,7 +2,10 @@ export const theme = {
   // brand colors
   dark: '#000',
   primaryBlue: '#1520A6',
+  secondaryBlue: '#4999e9',
   primaryRed: '#CF142B',
+  secondaryRed: '#ed4545',
+  primaryButton: '#E5AA12',
   background: '#f1efe7',
   white: '#fff',
   grey: '#8E9AAF',
