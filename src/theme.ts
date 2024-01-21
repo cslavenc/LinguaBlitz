4038d0cb31b4d8f48ea6f41027e5562702a0203f
slaven.cvijetic <slaven.cvijetic@noser.com>
2024-01-21 20:05:41 +0100

    Remove unused colors

--- src/theme.ts
+++ src/theme.ts
@@ -10,20 +10,4 @@ export const theme = {
   primaryButton: '#eeb011',
   disabled: '#f6d788',
   background: '#f1efe7',
-
-  // TODO : are these colors still required
-  turquoise: '#65D2B5',
-  orange: '#FFD6B1',
-  yellow: 'yellow',
-  autumngreen: '#D3E466',
-  green: '#CAF1BE',
-  violet: '#B5B1FF',
-  lightblue: '#ACDBF5',
-  blue: '#1D84B5',
-  // functional colors
-  active: '',
-  inactive: '',
-  // TODO : use brand colors here too?
-  save: '#A5FFA8',
-  cancel: '#FE8383',
 };
