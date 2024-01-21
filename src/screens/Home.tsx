375859c4211239f01a8a05a130e8e668840e9bb0
slaven.cvijetic <slaven.cvijetic@noser.com>
2024-01-21 14:14:55 +0100

    Add name and level to home screen

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,16 +1,48 @@
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { theme } from '../theme';
-import { RightArrow, UserIcon, VocabularyIcon } from '../components/Icons';
+import {
+  EditIcon,
+  RightArrow,
+  UserIcon,
+  VocabularyIcon,
+} from '../components/Icons';
 import { useNavigation } from '@react-navigation/native';
-import { CUSTOM_WORDS_KEY } from './custom/CustomWord';
+import { CUSTOM_WORDS_KEY } from '../utils';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { ACCOUNT_NAME_KEY, LEVEL_KEY } from './welcome/Welcome';
+import { useEffect, useState } from 'react';
 
 // TODO : add a dropdown where the level is chosen is saved into localStorage
 export const Home = () => {
   const navigation = useNavigation();
+  const [name, setName] = useState('');
+  const [level, setLevel] = useState('');
+
+  const getName = async () => {
+    const rawName = await AsyncStorage.getItem(ACCOUNT_NAME_KEY);
+    return rawName ? JSON.parse(rawName) : '';
+  };
+
+  const getLevel = async () => {
+    const rawLevel = await AsyncStorage.getItem(LEVEL_KEY);
+    return rawLevel ? JSON.parse(rawLevel) : '';
+  };
+
+  useEffect(() => {
+    getName().then((name) => setName(name));
+    getLevel().then((level) => setLevel(level));
+  }, []);
+
   return (
     <View style={styles.container}>
       <View style={styles.user}>
         <UserIcon size={100} color="grey" />
+      </View>
+      <View style={styles.edit}>
+        <Text style={styles.name}>{name}</Text>
+        <View style={styles.editIcon}>
+          <EditIcon />
+        </View>
       </View>
       <TouchableOpacity
         style={styles.card}
@@ -43,6 +75,12 @@ export const Home = () => {
           <RightArrow />
         </View>
       </TouchableOpacity>
+      <View style={[styles.card, { justifyContent: 'flex-start' }]}>
+        <View style={styles.icon}>
+          <VocabularyIcon />
+        </View>
+        <Text style={[styles.text, { marginLeft: 18 }]}>Level: {level}</Text>
+      </View>
     </View>
   );
 };
@@ -54,7 +92,23 @@ const styles = StyleSheet.create({
     justifyContent: 'flex-start',
     gap: 12,
   },
-  user: { alignSelf: 'center', paddingBottom: 24 },
+  user: { alignSelf: 'center' },
+  edit: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'flex-start',
+  },
+  editIcon: {
+    paddingTop: 10,
+  },
+  name: {
+    textAlign: 'center',
+    fontSize: 26,
+    fontWeight: 'bold',
+    paddingRight: 8,
+    paddingBottom: 18,
+  },
   card: {
     height: 48,
     backgroundColor: theme.white,
